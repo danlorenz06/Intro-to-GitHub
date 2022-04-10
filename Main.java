118bb6c07a438fa9c81b163d13a1e128afd4a4a7
Dan Lorenz Altaras <danlorenz06>
2022-04-10 20:38:04 +0800

    1st commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main	{
+				
+	public static void main(String[] args){
+		System.out.println("Wolfgang Mozart \n" );
+		System.out.println("Ludwig  Beethoven \n");
+		System.out.println("Johann  Bach   \n");
+		System.out.println("Frédéric Chopin    \n");
+		System.out.println("Claude Debussy    \n");
+}
